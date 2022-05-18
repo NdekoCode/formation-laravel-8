053e8d0747abfa97d6eebc1d923303510538daa9
Arick Bulakali <arickbulakali@gmail.com>
2022-05-18 20:36:00 +0200

    Add factory and fake data in database

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(5),
+            'content' => $this->faker->paragraphs(10, true),
+            'created_at' => $this->faker->date(),
+        ];
+    }
+}
