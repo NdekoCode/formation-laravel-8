ce5c81aabcc16014c9fd299039232186c1f201c5
Arick Bulakali <arickbulakali@gmail.com>
2022-05-18 20:19:45 +0200

    Add migration in databases

--- database/migrations/2022_05_18_181109_create_posts_table.php
+++ database/migrations/2022_05_18_181109_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 190);
+            $table->text('content');
             $table->timestamps();
         });
     }
