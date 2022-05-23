e92bf2b7f3621b46571cf08e4e2a00d36b1b4908
Arick Bulakali <arickbulakali@gmail.com>
2022-05-23 10:23:24 +0200

    Post comment relations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,9 @@ class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'content'];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
